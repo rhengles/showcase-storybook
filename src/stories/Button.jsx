d7ac873ab3c07ac61553bf567da8a7c9d4fcfe31
Rafael Hengles <rhengles@gmail.com>
2024-10-17 11:37:54 -0300

    Test intentionally broken - Change button prop 'label' to 'children'

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -5,29 +5,26 @@ import './button.css';
  * Primary UI component for user interaction
  */
 export const Button = (props) => {
-  props = mergeProps({ size: 'small' }, props);
-  const [local, rest] = splitProps(props, [
-    'primary',
-    'size',
-    'backgroundColor',
-    'label',
-  ]);
+	props = mergeProps({ size: 'small' }, props);
+	const [local, rest] = splitProps(props, [
+		'primary',
+		'size',
+		'backgroundColor',
+	]);
 
-  return (
-    <button
-      {...rest}
-      type="button"
-      classList={{
-        'storybook-button--small': local.size === 'small',
-        'storybook-button--medium': local.size === 'medium',
-        'storybook-button--large': local.size === 'large',
-        'storybook-button': true,
-        'storybook-button--primary': local.primary === true,
-        'storybook-button--secondary': local.primary === false,
-      }}
-      style={{ 'background-color': local.backgroundColor }}
-    >
-      {local.label}
-    </button>
-  );
+	return (
+		<button
+			type="button"
+			style={{ 'background-color': local.backgroundColor }}
+			{...rest}
+			classList={{
+				'storybook-button--small': local.size === 'small',
+				'storybook-button--medium': local.size === 'medium',
+				'storybook-button--large': local.size === 'large',
+				'storybook-button': true,
+				'storybook-button--primary': local.primary === true,
+				'storybook-button--secondary': local.primary === false,
+			}}
+		/>
+	);
 };
